a07cea61551e23ece0fe313cfeeb5bfbd8639c90
elkku26 <40237201+elkku26@users.noreply.github.com>
2018-06-14 11:46:19 +0300

    Add files via upload

--- player.py
+++ player.py
@@ -1,4 +1,6 @@
-from worldMap import world_map
+again = 0
+import time
+import worldMap
 import items
 class Player:
     def __init__(self):
@@ -12,6 +14,30 @@ class Player:
         self.x += dx
         self.y += dy
 
+    def move_north(self):
+        self.move(0, -1)
+        if worldMap.tile_at(self.x, self.y) is None:
+            self.move(0, 1)
+            worldMap.s_print("That's a wall. Try again.")
+            time.sleep(0.5)
+            worldMap.s_print("What will you do?")
+            again = 1
+
+    def move_south(self):
+        self.move(0,1)
+
+    def move_east(self):
+        self.move(1,0)
+
+    def move_west(self):
+        self.move(0, -1)
+        if worldMap.tile_at(self.x, self.y) is None:
+            self.move(0, 1)
+            worldMap.s_print("That's a wall. Try again.")
+            time.sleep(0.5)
+            worldMap.s_print("What will you do?")
+
+
     #get_location is to be used for debugging purposes only. get_x and get_y are for functions
     def get_location(self):
         return "X: {} Y: {}".format(self.x, self.y)
@@ -24,7 +50,17 @@ class Player:
 
 
     def get_room(self):
-         return world_map[self.y][self.x]
+         return worldMap.world_map[self.y][self.x]
 
     def check_inventory(self):
-        pass
+        for i in self.inventory:
+            worldMap.s_print("* " + i.__str__())
+        worldMap.s_print("What will you do?")
+
+    def check_condition(self):
+        if self.health > 15 or self.health == 15:
+            worldMap.s_print("You're slightly confused, but you aren't really hurt at all. You can definitely manage like this.")
+        if 10 < self.health < 15:
+            worldMap.s_print("You're feeling pretty bad. You can't survive long like this.")
+        if self.health < 10 or self.health == 10:
+            worldMap.s_print("You're in critical condition. You need help soon, or you'll perish.")
